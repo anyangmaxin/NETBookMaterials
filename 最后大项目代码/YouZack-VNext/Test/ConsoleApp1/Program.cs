7186c0a1632fc8265adb36e5dcdf77ef2c2e6847
马鑫 <anyangmaxin@163.com>
2022-11-03 10:32:55 +0800

    测试Unicode与UTF8中的占位数量

---
+++ 最后大项目代码/YouZack-VNext/Test/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a = "abcdefg我的世界";
+int i = System.Text.Encoding.Unicode.GetBytes(a).Length;
+//int i = System.Text.Encoding.UTF8.GetBytes(a).Length;
+
+int j = a.Length;
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(System.Text.Encoding.Unicode.GetBytes("abcdefg").Length);
+Console.WriteLine(System.Text.Encoding.Unicode.GetBytes("我的世界").Length);
